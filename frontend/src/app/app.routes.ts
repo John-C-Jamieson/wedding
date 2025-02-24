38b6ac34c773e3c4beeaa0ae704c4ac48e020398
John Jamieson <john@johnjamieson.ca>
2025-02-24 18:59:56 -0500

    Setup for future work

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'navbar-component', component: NavbarComponent },
+  { path: 'footer-component', component: FooterComponent },
+];
