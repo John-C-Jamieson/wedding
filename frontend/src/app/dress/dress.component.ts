fbd0601f892f4068e4eab9b45307250b264d8981
John Jamieson <john@johnjamieson.ca>
2025-02-25 18:08:08 -0500

    added all the pages

---
+++ frontend/src/app/dress/dress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dress',
+  imports: [],
+  templateUrl: './dress.component.html',
+  styleUrl: './dress.component.css'
+})
+export class DressComponent {
+
+}
