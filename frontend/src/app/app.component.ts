a4df18cd6b312443108c54540a308c4d06d6be96
John Jamieson <john@johnjamieson.ca>
2025-02-25 14:05:33 -0500

    Setting up navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { RouterLinkActive } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, FooterComponent],
+  imports:
+    [
+      RouterOutlet,
+      FooterComponent,
+      NavbarComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
-  weddingTitle: string = "John & Jane's Wedding";
+  weddingTitle: string = "John & Rosemary's Wedding";
   weddingDate: string = "Saturday, June 15, 2025";
   weddingLocation: string = "The Grand Venue, City, Country";
   description: string = "We are thrilled to invite you to our wedding celebration! Join us as we exchange vows and celebrate love with family and friends.";
--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports:
+    [
+      RouterLink,
+      RouterLinkActive
+    ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
