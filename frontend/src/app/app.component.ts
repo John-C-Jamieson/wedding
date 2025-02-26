9d1b563b2e8aa0b56ecc7b01bcc9b7c0fe52aac1
John Jamieson <john@johnjamieson.ca>
2025-02-26 12:52:29 -0500

    cleaned up app component imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { LocationComponent } from './location/location.component';
-import { DressComponent } from './dress/dress.component';
-import { MenuComponent } from './menu/menu.component';
-import { RegistryComponent } from './registry/registry.component';
-import { ScheduleComponent } from './schedule/schedule.component';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +9,7 @@ import { ScheduleComponent } from './schedule/schedule.component';
     [
       RouterOutlet,
       FooterComponent,
-      NavbarComponent,
-      HomeComponent,
-      LocationComponent,
-      DressComponent,
-      MenuComponent,
-      RegistryComponent,
-      ScheduleComponent,
+      NavbarComponent
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
